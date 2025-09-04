6ca8a9f307bd447ef9780447a4f86de38c02c247
kniz <kniz@byeol.io>
2025-09-04 23:37:27 +0900

    feat(roadmap): apply basic sprite & layer
    * sea layer and grassland layer to make an island

---
+++ assets/js/tileset.mjs
@@ -0,0 +1,55 @@
+export class layer {
+  constructor(map, zIndex) {
+    this.map = map;
+    this.zIndex = zIndex;
+  }
+
+  layoutTiles(elem) {
+    for(let y = 0; y < this.map.length; y++) {
+      const rowMap = this.map[y];
+      const row = this._makeRow();
+      elem.appendChild(row);
+
+      for(let x = 0; x < rowMap.length; x++) {
+        const tileId = rowMap[x];
+        if(tileId == undefined) continue;
+
+        row.appendChild(this._makeTile(tileId, x, y));
+      }
+    }
+  }
+
+  _makeRow() {
+    const row = document.createElement('div');
+    row.classList.add('layer-row');
+    return row;
+  }
+
+  _makeTile(tileId, x, y) {
+    const tile = document.createElement('img');
+    tile.classList.add('tile');
+    tile.style.zIndex = this.zIndex;
+    if(tileId < 0)
+      tile.style.visibility = "hidden";
+    else
+      tile.src = `../assets/images/tile/${tileId}.png`;
+
+    return tile
+  }
+}
+
+export class worldMap {
+  constructor(name, layers) {
+    this.name = name;
+    this.layers = layers;
+  }
+
+  layoutTiles(elem) {
+    this.layers.forEach((l, index) => {
+      const layerElem = document.createElement('div');
+      layerElem.classList.add("layer");
+      elem.appendChild(layerElem);
+      l.layoutTiles(layerElem);
+    });
+  }
+}
