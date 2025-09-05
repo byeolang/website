eedba294e94af260b5b9d5fdaddcdbd40f299a19
kniz <kniz@byeol.io>
2025-09-06 00:23:12 +0900

    feat(play): hide popup if you click out of popup

--- assets/js/result-popup.mjs
+++ assets/js/result-popup.mjs
@@ -10,6 +10,8 @@ export function onClickCloseButton() {
     var result = document.getElementById('result');
     result.classList.add('hidden-popup');
     result.style.opacity = '0';
+
+    window.removeEventListener('click', onClickGlobal);
 }
 
 export function showPopup() {
@@ -17,4 +19,15 @@ export function showPopup() {
     result.style.visibility = 'visible';
     result.style.opacity = '0.8';
     result.classList.remove('hidden-popup');
+
+    setTimeout(() => {
+      window.addEventListener('click', onClickGlobal);
+    }, 500);
+}
+
+function onClickGlobal(e) {
+  const result = document.getElementById('result');
+
+  if(!result.contains(e.target))
+    onClickCloseButton()
 }
