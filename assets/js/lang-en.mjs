2f91f85d8e5ae8efc19886d7566394bcbd7edda6
kniz <kniz@byeol.io>
2025-09-09 23:56:22 +0900

    feat(multilingual): apply ko/en for play

--- assets/js/lang-en.mjs
+++ assets/js/lang-en.mjs
@@ -12,5 +12,11 @@ export default {
   // Common UI elements
   "home": "home",
   "menu": "menu",
+
+  // play:
   "power": "power",
+  "verbose": "verbose",
+  "my codes": "my codes",
+  "hello world": "hello world",
+  "mike's clones": "mike's clones",
 };
