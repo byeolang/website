1e7c053d3d60b0d17242d11776c845465d959a4d
kniz <kniz@byeol.io>
2025-09-09 23:56:22 +0900

    feat(multilingual): apply ko/en for play

--- assets/js/lang-ko.mjs
+++ assets/js/lang-ko.mjs
@@ -2,15 +2,21 @@ export default {
   // Navigation tooltips
   "notice": "공지사항",
   "roadmap": "로드맵",
-  "download binary": "바이너리 다운로드",
-  "byeol language guide": "별 언어 가이드",
-  "playground": "플레이그라운드",
-  "c++ reference": "C++ 참조",
+  "download binary": "다운로드",
+  "byeol language guide": "언어 가이드",
+  "playground": "온라인 인터프리터",
+  "c++ reference": "C++ 레퍼런스",
   "toggle dark mode": "다크 모드 전환",
   "toggle language": "언어 전환",
 
   // Common UI elements
   "home": "홈",
   "menu": "메뉴",
+
+  // play:
   "power": "전원",
+  "verbose": "상세한 로깅",
+  "my codes": "내 코드",
+  "hello world": "헬로 월드",
+  "mike's clones": "복제 인간 마이크",
 };
