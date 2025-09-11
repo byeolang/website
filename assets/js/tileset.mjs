f33a88e11cb1e86312b1b180ad1cb2673dc77fa9
kniz <kniz@byeol.io>
2025-09-11 23:30:32 +0900

    refactor(roadmap): spped and memory improvements
    * reduced the number of tags by over 60% by generating <img> tags
      only where necessary.

--- assets/js/tileset.mjs
+++ assets/js/tileset.mjs
@@ -1,40 +1,38 @@
 export class layer {
   constructor(map) {
     this.map = map;
-    this.zIndex = 0;
   }
 
-  layoutTiles(elem, zIndex) {
-    this.zIndex = zIndex;
+  layoutTiles(parentElem, zIndex) {
+    const layerElem = document.createElement('div');
+    layerElem.classList.add("layer");
+    layerElem.style.height = `${4 * this.map.length}dvh`;
+    layerElem.style.zIndex = zIndex;
+
+    parentElem.appendChild(layerElem);
 
     for(let y = 0; y < this.map.length; y++) {
       const rowMap = this.map[y];
-      const row = this._makeRow();
-      elem.appendChild(row);
 
       for(let x = 0; x < rowMap.length; x++) {
         const tileId = rowMap[x];
         if(tileId == undefined) continue;
 
-        row.appendChild(this._makeTile(tileId, x, y));
+        const tile = this._makeTile(tileId, x, y);
+        if(tile != null)
+          layerElem.appendChild(tile);
       }
     }
   }
 
-  _makeRow() {
-    const row = document.createElement('div');
-    row.classList.add('layer-row');
-    return row;
-  }
-
   _makeTile(tileId, x, y) {
+    if(tileId < 0) return null;
+
     const tile = document.createElement('img');
     tile.classList.add('tile');
-    tile.style.zIndex = this.zIndex;
-    if(tileId < 0)
-      tile.style.visibility = "hidden";
-    else
-      tile.src = `../assets/images/tile/${tileId}.png`;
+    tile.style.left = `${x * 4}dvw`;
+    tile.style.top = `${y * 4}dvw`;
+    tile.src = `../assets/images/tile/${tileId}.png`;
 
     return tile
   }
@@ -49,10 +47,7 @@ export class worldMap {
 
   layoutTiles(elem) {
     this.layers.forEach((l, index) => {
-      const layerElem = document.createElement('div');
-      layerElem.classList.add("layer");
-      elem.appendChild(layerElem);
-      l.layoutTiles(layerElem, this.baseZIndex + index);
+      l.layoutTiles(elem, this.baseZIndex + index);
     });
   }
 }
