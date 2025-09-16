ad7f6560fdc5325ca9849d6f788dd8ea2cb2f296
kniz <kniz@byeol.io>
2025-09-16 23:30:38 +0900

    feat(roadmap): add scrollbar on description window

--- assets/js/roadmap_maps.mjs
+++ assets/js/roadmap_maps.mjs
@@ -225,7 +225,21 @@ const character = new tileset.characterLayer(
     new tileset.character(
       'milestone1',
       'nodeSprite',
-      'some description<br/>long long description<br/>very long description',
+      `
+        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/FChatxFF_XA' frameborder='0' allowfullscreen></iframe></div>
+        1some description<br/>
+        2long long description<br/>
+        3very long descriptionsome description<br/>
+        4long long description<br/>
+        5very long descriptionsome description<br/>
+        6long long description<br/>
+        7long long description<br/>
+        8very long descriptionsome description<br/>
+        9long long description<br/>
+        10very long descriptionsome description<br/>
+        11long long description<br/>
+        12very long description<br/>
+      `,
       13,
       13,
       '4dvw',
