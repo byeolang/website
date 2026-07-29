ffd1aa27b2797ae095208afc36842311183b60b2
autodocbot <kniz@byeol.io>
2026-07-29 10:09:42 +0000

    The our poor little Autobot \(❍ᴥ❍ʋ)/ generated docs for 45c9c5f5177da48b9fee48f893efce57a01a6885, clitter-clatter.

--- ref/generated/en/search/classes_6.js
+++ ref/generated/en/search/classes_6.js
@@ -1,15 +1,6 @@
 var searchData=
 [
-  ['fboexpr_0',['FBOExpr',['../classby_1_1_f_b_o_expr.html',1,'by']]],
-  ['fileflag_1',['fileFlag',['../classby_1_1file_flag.html',1,'by']]],
-  ['filesupply_2',['fileSupply',['../classby_1_1file_supply.html',1,'by']]],
-  ['forexpr_3',['forExpr',['../classby_1_1for_expr.html',1,'by']]],
-  ['frame_4',['frame',['../classby_1_1frame.html',1,'by']]],
-  ['frameinteract_5',['frameInteract',['../classby_1_1frame_interact.html',1,'by']]],
-  ['frameinteractable_6',['frameInteractable',['../classby_1_1frame_interactable.html',1,'by']]],
-  ['frames_7',['frames',['../classby_1_1frames.html',1,'by']]],
-  ['frontendstacker_8',['frontendStacker',['../classby_1_1frontend_stacker.html',1,'by']]],
-  ['func_9',['func',['../classby_1_1func.html',1,'by']]],
-  ['functype_10',['funcType',['../classby_1_1func_type.html',1,'by']]],
-  ['fuoexpr_11',['FUOExpr',['../classby_1_1_f_u_o_expr.html',1,'by']]]
+  ['id_0',['id',['../structby_1_1id.html',1,'by']]],
+  ['instance_1',['instance',['../classby_1_1instance.html',1,'by']]],
+  ['instancer_2',['instancer',['../classby_1_1instancer.html',1,'by']]]
 ];
--- ref/generated/en/search/files_b.js
+++ ref/generated/en/search/files_b.js
@@ -1,9 +1,6 @@
 var searchData=
 [
-  ['manifest_2ehpp_0',['manifest.hpp',['../manifest_8hpp.html',1,'']]],
-  ['marshaling_2ehpp_1',['marshaling.hpp',['../marshaling_8hpp.html',1,'']]],
-  ['mgd_2ehpp_2',['mgd.hpp',['../mgd_8hpp.html',1,'']]],
-  ['mgdtype_2ehpp_3',['mgdType.hpp',['../mgd_type_8hpp.html',1,'']]],
-  ['modifier_2ehpp_4',['modifier.hpp',['../modifier_8hpp.html',1,'(Global Namespace)'],['../modifier_2modifier_8hpp.html',1,'(Global Namespace)']]],
-  ['modules_2edox_5',['modules.dox',['../modules_8dox.html',1,'']]]
+  ['parser_2ehpp_0',['parser.hpp',['../parser_8hpp.html',1,'']]],
+  ['point_2ehpp_1',['point.hpp',['../point_8hpp.html',1,'']]],
+  ['pool_2ehpp_2',['pool.hpp',['../pool_8hpp.html',1,'']]]
 ];
