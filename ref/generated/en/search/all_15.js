b996d4d6df6c53d5e810ff706763f42c9ea8a519
autodocbot <kniz@byeol.io>
2026-07-30 06:15:19 +0000

    The our poor little Autobot \(❍ᴥ❍ʋ)/ generated docs for 71bc4c2de54a0eba8f5943b6148ba014d99e9299, clitter-clatter.

--- ref/generated/en/search/all_15.js
+++ ref/generated/en/search/all_15.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['y_20lifecycle_20management_0',['lowparser.y Lifecycle Management',['../classby_1_1parser.html#lowparser_lifecycle_management',1,'']]],
-  ['your_20rule_20after_20using_20dispatcher_1',['must return token in your rule after using dispatcher',['../classby_1_1token_dispatcher.html#you',1,'']]]
+  ['you_20use_20by_5f_20_5fscope_20macro_20in_20if_20block_20statement_0',['careful when you use BY_?_SCOPE macro in &apos;if&apos; block statement',['../clog_2common_2macro_8hpp.html#be',1,'']]]
 ];
