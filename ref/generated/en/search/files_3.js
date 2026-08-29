631e685a6a0e19c9fb007443168cb1378413024c
autodocbot <kniz@byeol.io>
2026-08-29 08:29:30 +0000

    The our poor little Autobot \(❍ᴥ❍ʋ)/ generated docs for d8075a7e92ce4b6c8422a53dbe158b61e91aff47, clitter-clatter.

--- ref/generated/en/search/files_3.js
+++ ref/generated/en/search/files_3.js
@@ -1,11 +1,8 @@
 var searchData=
 [
-  ['cli_2ehpp_0',['cli.hpp',['../cli_8hpp.html',1,'']]],
-  ['closure_2ehpp_1',['closure.hpp',['../closure_8hpp.html',1,'']]],
-  ['common_2ehpp_2',['common.hpp',['../core_2common_8hpp.html',1,'(Global Namespace)'],['../frontend_2common_8hpp.html',1,'(Global Namespace)']]],
-  ['container_2ehpp_3',['container.hpp',['../container_8hpp.html',1,'']]],
-  ['convergence_2ehpp_4',['convergence.hpp',['../convergence_8hpp.html',1,'(Global Namespace)'],['../convergence_2convergence_8hpp.html',1,'(Global Namespace)']]],
-  ['cpp_2ehpp_5',['cpp.hpp',['../bridge_2cpp_8hpp.html',1,'(Global Namespace)'],['../loader_2pod_2cpp_8hpp.html',1,'(Global Namespace)']]],
-  ['cpppodloading_2ehpp_6',['cppPodLoading.hpp',['../cpp_pod_loading_8hpp.html',1,'']]],
-  ['ctor_2ehpp_7',['ctor.hpp',['../ctor_8hpp.html',1,'(Global Namespace)'],['../ctor_2ctor_8hpp.html',1,'(Global Namespace)']]]
+  ['chunk_2ehpp_0',['chunk.hpp',['../chunk_8hpp.html',1,'']]],
+  ['chunks_2ehpp_1',['chunks.hpp',['../chunks_8hpp.html',1,'']]],
+  ['clonable_2ehpp_2',['clonable.hpp',['../clonable_8hpp.html',1,'']]],
+  ['common_2ehpp_3',['common.hpp',['../clog_2common_8hpp.html',1,'(Global Namespace)'],['../memlite_2common_8hpp.html',1,'(Global Namespace)'],['../meta_2common_8hpp.html',1,'(Global Namespace)'],['../stela_2common_8hpp.html',1,'(Global Namespace)']]],
+  ['consolestream_2ehpp_4',['consoleStream.hpp',['../console_stream_8hpp.html',1,'']]]
 ];
--- ref/generated/en/search/functions_3.js
+++ ref/generated/en/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['clonechain_0',['cloneChain',['../classby_1_1tnchain.html#ab7302f235f4625e88510cee8624e502c',1,'by::tnchain::cloneChain(const super *until) const'],['../classby_1_1tnchain.html#a3f59076394d4c93b5dab8962ea85fc48',1,'by::tnchain::cloneChain() const']]],
-  ['converge_1',['converge',['../classby_1_1tbase_convergence.html#a05419a357daf3f8601475b4f47ee07a4',1,'by::tbaseConvergence']]],
-  ['countdown_2',['countDown',['../classby_1_1smart_dedent.html#ab821f295c4571b5792a57dbde07f87c9',1,'by::smartDedent']]],
-  ['countup_3',['countUp',['../classby_1_1smart_dedent.html#a9a8fbaaaaac0a358fc17641daa5dec48',1,'by::smartDedent']]]
+  ['canbind_0',['canBind',['../classby_1_1binder.html#a62644cd115f0e1dc528fde6447e2f0a0',1,'by::binder']]],
+  ['cast_1',['cast',['../classby_1_1type_providable.html#ad05c9ba2164853c3e8db3af3cb5198c3',1,'by::typeProvidable::cast()'],['../classby_1_1binder.html#ad05c9ba2164853c3e8db3af3cb5198c3',1,'by::binder::cast()']]],
+  ['countdown_2',['countDown',['../classby_1_1stela_smart_dedent.html#af6c1a6369f8f5a6be1d7c1e000989d19',1,'by::stelaSmartDedent']]],
+  ['countup_3',['countUp',['../classby_1_1stela_smart_dedent.html#a7bc29e06927de235262827df9d1211ec',1,'by::stelaSmartDedent']]]
 ];
