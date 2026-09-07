384168440af51d0b3d7baa8a20cdd0ecddda7a67
autodocbot <kniz@byeol.io>
2026-09-07 10:44:55 +0000

    The our poor little Autobot \(❍ᴥ❍ʋ)/ generated docs for 3a44fcbb518d8ca5b462efb2c43a0ecd56ee5de8, clitter-clatter.

--- ref/generated/en/search/files_b.js
+++ ref/generated/en/search/files_b.js
@@ -1,11 +1,6 @@
 var searchData=
 [
-  ['macro_2ehpp_0',['macro.hpp',['../indep__dep-src_2module_2indep_2macro_8hpp.html',1,'(Global Namespace)'],['../stela__dep-src_2module_2clog_2common_2macro_8hpp.html',1,'(Global Namespace)'],['../stela__dep-src_2module_2meta_2common_2macro_8hpp.html',1,'(Global Namespace)'],['../stela__dep-src_2module_2stela_2common_2macro_8hpp.html',1,'(Global Namespace)']]],
-  ['manifest_2ehpp_1',['manifest.hpp',['../manifest_8hpp.html',1,'']]],
-  ['marshaling_2ehpp_2',['marshaling.hpp',['../marshaling_8hpp.html',1,'']]],
-  ['memoryhaver_2ehpp_3',['memoryHaver.hpp',['../memory_haver_8hpp.html',1,'']]],
-  ['mgd_2ehpp_4',['mgd.hpp',['../mgd_8hpp.html',1,'']]],
-  ['mgdtype_2ehpp_5',['mgdType.hpp',['../mgd_type_8hpp.html',1,'']]],
-  ['modifier_2ehpp_6',['modifier.hpp',['../modifier_8hpp.html',1,'(Global Namespace)'],['../modifier_2modifier_8hpp.html',1,'(Global Namespace)']]],
-  ['modules_2edox_7',['modules.dox',['../modules_8dox.html',1,'']]]
+  ['parser_2ehpp_0',['parser.hpp',['../parser_8hpp.html',1,'']]],
+  ['point_2ehpp_1',['point.hpp',['../point_8hpp.html',1,'']]],
+  ['pool_2ehpp_2',['pool.hpp',['../pool_8hpp.html',1,'']]]
 ];
